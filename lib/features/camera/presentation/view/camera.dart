f2925078d0bd011716c49b3c36a2767b599f8a36
Moataz Tabl <moataztabl.m2@gmail.com>
2024-04-15 01:15:34 +0200

    renamed folders and camera.dart

--- lib/features/camera/presentation/view/camera.dart
+++ lib/features/camera/presentation/view/camera.dart
@@ -632,7 +632,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
       CameraDescription cameraDescription) async {
     final CameraController cameraController = CameraController(
       cameraDescription,
-      kIsWeb ? ResolutionPreset.max : ResolutionPreset.medium,
+      kIsWeb ? ResolutionPreset.max : ResolutionPreset.max,
       enableAudio: enableAudio,
       imageFormatGroup: ImageFormatGroup.jpeg,
     );
