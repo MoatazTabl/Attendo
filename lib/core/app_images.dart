d93b3fb75131c0e8c25f366a67d74326ec9909c9
Moaz <moazomran20@gmail.com>
2024-04-17 18:12:19 +0200

    instructor sign in  page image added

--- lib/core/app_images.dart
+++ lib/core/app_images.dart
@@ -8,6 +8,9 @@ abstract class AppImages {
   static const String welcomePageAdminImage =
       "assets/images/svg/welcomePageAdmin.svg";
 
+  static const String welcomePageInstructorImage =
+      "assets/images/svg/welcome_instructor.svg";
+
   static const String studentIntro = 'assets/images/svg/student_intro.svg';
   static const String instructorIntro = 'assets/images/svg/instructor_intro.svg';
   static const String adminIntro = 'assets/images/svg/admin_intro.svg';
