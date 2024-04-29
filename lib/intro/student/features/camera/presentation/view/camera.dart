8cca38f6ba8ccf1bb412657d896a5ab72184e916
Moaz <moazomran20@gmail.com>
2024-04-29 20:37:14 +0200

    blur edited

--- lib/intro/student/features/camera/presentation/view/camera.dart
+++ lib/intro/student/features/camera/presentation/view/camera.dart
@@ -63,12 +63,10 @@ class _CameraScreenState extends State<CameraScreen>
                   SizedBox(
                     width: 1.sw,
                     height: 0.3.sh,
-                    child: Positioned.fill(
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                        child: Container(
-                          color: Colors.white10.withOpacity(0.2),
-                        ),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+                      child: Container(
+                        color: Colors.white10.withOpacity(0.3),
                       ),
                     ),
                   ),
@@ -77,12 +75,10 @@ class _CameraScreenState extends State<CameraScreen>
                     child: SizedBox(
                       width: 1.sw,
                       height: 0.3.sh,
-                      child: Positioned.fill(
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                          child: Container(
-                            color: Colors.white10.withOpacity(0.2),
-                          ),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+                        child: Container(
+                          color: Colors.white10.withOpacity(0.3),
                         ),
                       ),
                     ),
