2a70b674252343b8a65c42880d9502e4ac71aedb
Moataz Tabl <moataztabl.m2@gmail.com>
2024-05-01 16:37:35 +0300

    added api_strings.dart

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,20 +1,21 @@
+import 'package:attendo/core/networking/api_strings.dart';
 import 'package:dio/dio.dart';
 
 class ApiService {
-  final String _baseUrl = "http://0.tcp.eu.ngrok.io:18834/";
+
 
   final Dio _dio = Dio();
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
     var response = await _dio.get(
-      "$_baseUrl$endpoint",
+      "${ApiStrings.baseUrl}$endpoint",
     );
     return response.data;
   }
 
   Future<Map<String,dynamic>> post({required String endpoint,required Map data})async
   {
-    var response= await _dio.post("$_baseUrl$endpoint",data: data);
+    var response= await _dio.post("${ApiStrings.baseUrl}$endpoint",data: data);
     return response.data;
   }
 }
