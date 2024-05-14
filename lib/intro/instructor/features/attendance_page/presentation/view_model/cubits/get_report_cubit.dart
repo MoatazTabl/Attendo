b4cf43b19c8b278a051da9dc599035d8b4c4fc5a
Moaz <moazomran20@gmail.com>
2024-05-15 00:47:10 +0300

    no students list updated

--- lib/intro/instructor/features/attendance_page/presentation/view_model/cubits/get_report_cubit.dart
+++ lib/intro/instructor/features/attendance_page/presentation/view_model/cubits/get_report_cubit.dart
@@ -1,29 +1,29 @@
 import 'package:attendo/core/networking/api_service.dart';
 import 'package:attendo/core/networking/api_strings.dart';
 import 'package:attendo/intro/instructor/features/attendance_page/presentation/view_model/models/GetReportModel.dart';
+import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../../../../../../core/errors/failures.dart';
-
+import 'package:meta/meta.dart';
 part 'get_report_state.dart';
 
 class GetReportCubit extends Cubit<GetReportState> {
   GetReportCubit() : super(GetReportInitial());
 
-  getReport(int lecturePk, String date) async {
+  getReport(int lecturePk,String date) async {
     try {
       emit(GetReportLoading());
-      final response = await ApiService().post(
-          endpoint: ApiStrings.getReport,
-          data: {"lecturepk": lecturePk, "date": date});
-      final report = GetReportModel.fromJson(response);
-      emit(GetReportSuccess(getReportModel: report));
+     final response =
+          await ApiService().post(endpoint: ApiStrings.getReport, data: {
+            "lecturepk": lecturePk,
+            "date": date
+          });
+     final report = GetReportModel.fromJson(response);
+     emit(GetReportSuccess(getReportModel: report));
     } on Exception catch (e) {
       if (e is DioException) {
-        final k = ServerFailures.fromDioException(e);
-        emit(GetReportFailure(errMessage: k.errorMessage));
+        emit(GetReportFailure(errMessage: "No Students"));
       } else {
         emit(GetReportFailure(errMessage: "Un Expected error , try again"));
       }
