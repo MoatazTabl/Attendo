ef1a940f9bdffa3838bc3a7e2ce90090d77d0988
Moaz <moazomran20@gmail.com>
2024-06-21 17:19:47 +0300

    get all students request finished

--- lib/intro/admin/features/all_students_screen/view_model/cubit/get_all_students_cubit.dart
+++ lib/intro/admin/features/all_students_screen/view_model/cubit/get_all_students_cubit.dart
@@ -1,19 +1,36 @@
 import 'package:attendo/core/networking/api_service.dart';
 import 'package:attendo/core/networking/api_strings.dart';
+import 'package:attendo/intro/admin/features/all_students_screen/view_model/model/AllStudentsModel.dart';
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
+
+import '../../../../../../core/errors/failures.dart';
 
 part 'get_all_students_state.dart';
 
 class GetAllStudentsCubit extends Cubit<GetAllStudentsState> {
   GetAllStudentsCubit() : super(GetAllStudentsInitial());
 
-  getAllStudents(String department , String grade) {
-    final response =
-        ApiService().post(endpoint: ApiStrings.getAllStudents, data: {
-          "faculty": department,
-          "grade": grade
-        });
-
+  getAllStudents(String department, String grade) async {
+    try {
+      emit(GetAllStudentsLoading());
+      var response = await ApiService().post(
+          endpoint: ApiStrings.getAllStudents,
+          data: {"faculty": department, "grade": grade});
+      List<AllStudentsModel> students = [];
+      for (int i = 0; i <= response.length; i++) {
+        students.add(AllStudentsModel.fromJson(response[i]));
+      }
+      emit(GetAllStudentsSuccess());
+    } on Exception catch (e) {
+      if (e is DioException) {
+        final k = ServerFailures.fromDioException(e);
+        emit(GetAllStudentsFailure(errMessage: k.errorMessage));
+      } else {
+        emit(
+            GetAllStudentsFailure(errMessage: "Un Expected error , try again"));
+      }
+    }
   }
 }
