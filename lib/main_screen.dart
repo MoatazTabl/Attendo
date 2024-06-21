9475022a867bb962041ef2e53751e6917cef483f
moataz <moataztabl.m2@gmail.com>
2024-06-21 05:09:40 +0300

    Finished bottom_nav_bar

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -4,8 +4,9 @@ import 'package:attendo/intro/auth/models/user_data_model.dart';
 import 'package:attendo/intro/student/features/home/Presentation/view/home_screen_student.dart';
 import 'package:attendo/intro/student/features/profile/Presentation/view/profile_screen_student.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 import 'core/app_images.dart';
 import 'intro/settings/presentation/view/settings_screen.dart';
@@ -41,52 +42,94 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: screens[index]),
-      bottomNavigationBar: Theme(
-        data: AppTheme.lightMode.copyWith(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-        ),
-        child: BottomNavigationBar(
-          currentIndex: index,
-          iconSize: 32.w,
-          elevation: 2,
-          backgroundColor: Colors.white,
-          items: [
-            BottomNavigationBarItem(
+      bottomNavigationBar: StylishBottomBar(
+        items: [
+          BottomBarItem(
               icon: SvgPicture.asset(
                 AppImages.homeIcon,
               ),
-              activeIcon: SvgPicture.asset(
-                AppImages.selectedHomeIcon,
+              title: Text(
+                getAppLocalizations(context)!.home,
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
               ),
-              label: getAppLocalizations(context)!.home,
-            ),
-            BottomNavigationBarItem(
+              backgroundColor: AppTheme.mainBlue),
+          BottomBarItem(
               icon: SvgPicture.asset(
                 AppImages.profileIcon,
               ),
-              activeIcon: SvgPicture.asset(
-                AppImages.selectedProfileIcon,
+              title: Text(
+                getAppLocalizations(context)!.profile,
+                style: GoogleFonts.poppins(color: Colors.white, fontSize: 12),
               ),
-              label: getAppLocalizations(context)!.profile,
-            ),
-            BottomNavigationBarItem(
-              label: getAppLocalizations(context)!.settings,
+              backgroundColor: AppTheme.mainBlue),
+          BottomBarItem(
+              title: Text(
+                getAppLocalizations(context)!.settings,
+                style: GoogleFonts.poppins(color: Colors.white, fontSize: 12),
+              ),
               icon: SvgPicture.asset(
                 AppImages.settingsIcon,
               ),
-              activeIcon: SvgPicture.asset(
-                AppImages.selectedSettingsIcon,
-                fit: BoxFit.fill,
-              ),
-            ),
-          ],
-          onTap: (value) {
-            index = value;
-            setState(() {});
-          },
+              backgroundColor: AppTheme.mainBlue)
+        ],
+        option: BubbleBarOptions(
+          barStyle: BubbleBarStyle.horizontal,
         ),
+        currentIndex: index,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
       ),
     );
   }
 }
+//bottomNavigationBar: Theme(
+//   data: AppTheme.lightMode.copyWith(
+//     splashColor: Colors.transparent,
+//     highlightColor: Colors.transparent,
+//   ),
+//   child: BottomNavigationBar(
+//     currentIndex: index,
+//     iconSize: 32.w,
+//     elevation: 2,
+//     backgroundColor: Colors.white,
+//     items: [
+//       BottomNavigationBarItem(
+//         icon: SvgPicture.asset(
+//           AppImages.homeIcon,
+//         ),
+//         activeIcon: SvgPicture.asset(
+//           AppImages.selectedHomeIcon,
+//         ),
+//         label: getAppLocalizations(context)!.home,
+//       ),
+//       BottomNavigationBarItem(
+//         icon: SvgPicture.asset(
+//           AppImages.profileIcon,
+//         ),
+//         activeIcon: SvgPicture.asset(
+//           AppImages.selectedProfileIcon,
+//         ),
+//         label: getAppLocalizations(context)!.profile,
+//       ),
+//       BottomNavigationBarItem(
+//         label: getAppLocalizations(context)!.settings,
+//         icon: SvgPicture.asset(
+//           AppImages.settingsIcon,
+//         ),
+//         activeIcon: SvgPicture.asset(
+//           AppImages.selectedSettingsIcon,
+//           fit: BoxFit.fill,
+//         ),
+//       ),
+//     ],
+//     onTap: (value) {
+//       index = value;
+//       setState(() {});
+//     },
+//   ),
