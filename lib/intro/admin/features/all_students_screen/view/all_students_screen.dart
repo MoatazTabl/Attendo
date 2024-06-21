092df25ea0d8a315c5cf6671727cf4d092ab8bdc
Moaz <moazomran20@gmail.com>
2024-06-22 02:03:02 +0300

    student details screen added

--- lib/intro/admin/features/all_students_screen/view/all_students_screen.dart
+++ lib/intro/admin/features/all_students_screen/view/all_students_screen.dart
@@ -3,6 +3,9 @@ import 'package:attendo/intro/admin/features/home/models/students_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../../core/networking/api_strings.dart';
 
 class AllStudentsScreen extends StatefulWidget {
   final StudentsModel studentsModel;
@@ -30,41 +33,51 @@ class _AllStudentsScreenState extends State<AllStudentsScreen> {
         child: BlocBuilder<GetAllStudentsCubit, GetAllStudentsState>(
           builder: (context, state) {
             if (state is GetAllStudentsSuccess) {
+              if(state.students.isEmpty)
+                {
+                  return const Center(child: Text("No Students found"));
+                }
               return Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: GridView.builder(
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    crossAxisSpacing: 50,
-                    mainAxisSpacing: 30,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 25,
                     mainAxisExtent: 150,
                   ),
                   physics: const ClampingScrollPhysics(),
                   itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFE8E8E8),
-                          borderRadius: BorderRadius.circular(20)),
-                      height: 50,
-                      width: 50,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 9.h,
-                          ),
-                          CircleAvatar(
-                            backgroundImage: NetworkImage(state.students[index].photo??""),
-                            radius: 40,
-                          ),
-                          SizedBox(
-                            height: 9.h,
-                          ),
-                          Text(state.students[index].name!),
-                          SizedBox(
-                            height: 5.h,
-                          ),
-                          Text(state.students[index].nationalId!),
-                        ],
+                    return GestureDetector(
+                      onTap: () {
+                        context.push("/studentDetails",extra: state.students[index]);
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: const Color(0xFFE8E8E8),
+                            borderRadius: BorderRadius.circular(40)),
+                        height: 50,
+                        width: 50,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 9.h,
+                            ),
+                            CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  "${ApiStrings.baseUrl}${state.students[index].photo}"),
+                              radius: 40,
+                            ),
+                            SizedBox(
+                              height: 9.h,
+                            ),
+                            Text(state.students[index].name!),
+                            SizedBox(
+                              height: 5.h,
+                            ),
+                            Text(state.students[index].nationalId!),
+                          ],
+                        ),
                       ),
                     );
                   },
---
+++ lib/intro/admin/features/student_details/presentation/view/student_details_screen.dart
@@ -0,0 +1,132 @@
+import 'package:attendo/intro/admin/features/all_students_screen/view_model/model/AllStudentsModel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../../../core/app_images.dart';
+import '../../../../../../core/helpers/common.dart';
+import '../../../../../../core/networking/api_strings.dart';
+import '../../../../../../core/widgets/custom_drop_down_button.dart';
+import '../../../../../../core/widgets/custom_form_elevated_button.dart';
+import '../../../../../../core/widgets/text_form_field.dart';
+
+class StudentDetailsScreen extends StatelessWidget {
+  AllStudentsModel studentDetails;
+
+  StudentDetailsScreen({super.key, required this.studentDetails});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 52.h,
+              ),
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "${ApiStrings.baseUrl}${studentDetails.photo}"),
+                radius: 75,
+              ),
+              SizedBox(
+                height: 32.h,
+              ),
+              Text(
+                studentDetails.name!,
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+              SizedBox(
+                height: 32.h,
+              ),
+              CustomTextFormField(
+                isPass: false,
+                prefixIcon: SvgPicture.asset(AppImages.nameIcon),
+                textInputType: TextInputType.name,
+                autofillHints: const [AutofillHints.name],
+                textFieldLabel: getAppLocalizations(context)!.firstName,
+              ),
+              SizedBox(
+                height: 28.h,
+              ),
+              CustomTextFormField(
+                isPass: false,
+                prefixIcon: SvgPicture.asset(AppImages.emailIcon),
+                onChanged: (value) {
+                },
+                textInputType: TextInputType.emailAddress,
+                autofillHints: const [AutofillHints.email],
+                textFieldLabel:
+                getAppLocalizations(context)!.universityEmail,
+              ),
+              SizedBox(
+                height: 28.h,
+              ),
+              CustomTextFormField(
+                isPass: false,
+                prefixIcon: SvgPicture.asset(
+                  AppImages.nationalIdIcon,
+                  height: 22,
+                  width: 22,
+                ),
+                textInputType: TextInputType.number,
+                textFieldLabel: getAppLocalizations(context)!.nationalID,
+              ),
+              SizedBox(
+                height: 28.h,
+              ),
+              CustomFormDropDownButton(
+                initialValue: studentDetails.faculty,
+                type: "faculty",
+                fieldHint: getAppLocalizations(context)!.selectFaculty,
+                list: const ["Computers", "Commerce"],
+                onValueChanged: (selectedValue) {
+
+                },
+              ),
+
+              CustomFormDropDownButton(
+                initialValue: studentDetails.grade!,
+                type: studentDetails.grade!,
+                fieldHint: getAppLocalizations(context)!.selectGrade,
+                list: const ["First", "Second", "Third", "Fourth"],
+                onValueChanged: (selectedValue) {
+                },
+              ),
+          ElevatedButton(
+            style: ButtonStyle(
+              fixedSize: WidgetStateProperty.all(
+                Size(240.w, 56.h),
+              ),
+              backgroundColor: WidgetStateProperty.all(
+                const Color(
+                  0xff0066FF,
+                ),
+              ),
+              foregroundColor: WidgetStateProperty.all(Colors.white),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            onPressed: () {
+
+            },
+            child: Text(
+              "Update Information",
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                fontSize: 16.sp,
+              ),
+            ),
+          ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
