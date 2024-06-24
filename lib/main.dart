6f7cbb91d842e0d1b9f3fce4c4ff01cca072468a
moataz <moataztabl.m2@gmail.com>
2024-06-24 14:57:56 +0300

    removed sentry

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mobile_device_identifier/mobile_device_identifier.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'core/helpers/cache_helper.dart';
 import 'core/helpers/preference_helper.dart';
@@ -16,25 +15,11 @@ Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await mainFunctions(widgetsBinding);
   CacheHelper().init();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((value) async {
-    await SentryFlutter.init(
-          (options) {
-        options.dsn =
-        'https://d8b83470ba65e60209cf6ef175f1cd99@o4507479302799360.ingest.de.sentry.io/4507479304503376';
-        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-        // We recommend adjusting this value in production.
-        options.tracesSampleRate = 1.0;
-        // The sampling rate for profiling is relative to tracesSampleRate
-        // Setting to 1.0 will profile 100% of sampled transactions:
-        options.profilesSampleRate = 1.0;
-      },
-      appRunner: () =>
-          runApp(
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (value) => runApp(
             const Attendo(),
           ),
-    );
-  });
+  );
 }
 
 Future<void> mainFunctions(WidgetsBinding widgetsBinding) async {
