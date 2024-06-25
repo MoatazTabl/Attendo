6c91b68584ba5059d4b4b475f10101db968a9a1b
Moaz <moazomran20@gmail.com>
2024-06-25 06:14:07 +0300

    excell file added

--- lib/intro/instructor/features/report_history/presentation/view/lecture_report_page.dart
+++ lib/intro/instructor/features/report_history/presentation/view/lecture_report_page.dart
@@ -6,6 +6,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../../../../../../core/app_images.dart';
 import '../../../../../../core/helpers/common.dart';
+import '../../../../../../core/widgets/custom_snack_bar.dart';
+import '../../../attendance_page/presentation/view/widgets/excell_functions.dart';
 import '../view_model/cubits/get_lectures_reports_cubit.dart';
 
 class LectureReportPage extends StatefulWidget {
@@ -35,111 +37,119 @@ class _LectureReportPageState extends State<LectureReportPage> {
                 BlocBuilder<GetLecturesReportsCubit, GetLecturesReportsState>(
               builder: (context, state) {
                 if (state is GetLecturesReportsSuccess) {
-                 if(state.reportModel.isEmpty)
-                   {
-                     return const Center(child: Text("No Data"));
-                   }
-                 else
-                   {
-                     return Column(
-                       crossAxisAlignment: CrossAxisAlignment.center,
-                       children: [
-                         Row(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                             Column(
-                               children: [
-                                 Text(
-                                   state.reportModel[0].lectureName ?? "",
-                                   style: Theme.of(context)
-                                       .textTheme
-                                       .titleLarge!
-                                       .copyWith(
-                                     fontSize: 30.sp,
-                                   ),
-                                 ),
-                               ],
-                             ),
-                           ],
-                         ),
-                         Container(
-                           height: 584.h,
-                           width: 328.w,
-                           decoration: BoxDecoration(
-                             borderRadius: BorderRadius.circular(
-                               23,
-                             ),
-                             color: const Color(
-                               0xFFF1F1F1,
-                             ),
-                           ),
-                           child: ListView.builder(
-                             itemBuilder: (context, index) {
-                               return Padding(
-                                 padding: const EdgeInsets.only(top: 8.0),
-                                 child: ListTile(
-                                     leading: SvgPicture.asset(
-                                       AppImages.studentAvatarTest,
-                                       width: 50.w,
-                                     ),
-                                     title: Text(
-                                       state.reportModel[0].studentsList?[index]
-                                           .name ??
-                                           "",
-                                       style: const TextStyle(color: Colors.black),
-                                     ),
-                                     subtitle: Row(
-                                       mainAxisAlignment:
-                                       MainAxisAlignment.spaceBetween,
-                                       children: [
-                                         Text(
-                                           "ID:${state.reportModel[0].studentsList?[index].userId ?? ""}",
-                                           style: Theme.of(context)
-                                               .textTheme
-                                               .titleSmall!
-                                               .copyWith(fontSize: 12),
-                                         )
-                                       ],
-                                     ),
-                                     trailing: Text(state
-                                         .reportModel[0].authorizationTime
-                                         .toString())),
-                               );
-                             },
-                             itemCount: state.reportModel[0].studentsList?.length,
-                           ),
-                         ),
-                         const SizedBox(
-                           height: 20,
-                         ),
-                         ElevatedButton(
-                           onPressed: () async {},
-                           style: ButtonStyle(
-                             fixedSize: WidgetStateProperty.all(
-                               Size(246.w, 53.h),
-                             ),
-                             shape: WidgetStateProperty.all(
-                               RoundedRectangleBorder(
-                                 borderRadius: BorderRadius.circular(
-                                   16.w,
-                                 ),
-                               ),
-                             ),
-                           ),
-                           child: Text(
-                             getAppLocalizations(context)!.downloadFullList,
-                             style: GoogleFonts.roboto(
-                               textStyle: TextStyle(
-                                 fontSize: 20.sp,
-                                 fontWeight: FontWeight.w500,
-                                 color: Colors.white,
-                               ),
-                             ),
-                           ),
-                         ),
-                       ],
-                     );
-                   }
+                  if (state.reportModel.isEmpty) {
+                    return const Center(child: Text("No Data"));
+                  } else {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  state.reportModel[0].lectureName ?? "",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleLarge!
+                                      .copyWith(
+                                        fontSize: 30.sp,
+                                      ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 584.h,
+                          width: 328.w,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              23,
+                            ),
+                            color: const Color(
+                              0xFFF1F1F1,
+                            ),
+                          ),
+                          child: ListView.builder(
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: ListTile(
+                                    leading: SvgPicture.asset(
+                                      AppImages.studentAvatarTest,
+                                      width: 50.w,
+                                    ),
+                                    title: Text(
+                                      state.reportModel[0].studentsList?[index]
+                                              .name ??
+                                          "",
+                                      style:
+                                          const TextStyle(color: Colors.black),
+                                    ),
+                                    subtitle: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "ID:${state.reportModel[0].studentsList?[index].userId ?? ""}",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleSmall!
+                                              .copyWith(fontSize: 12),
+                                        )
+                                      ],
+                                    ),
+                                    trailing: Text(state
+                                        .reportModel[0].authorizationTime
+                                        .toString())),
+                              );
+                            },
+                            itemCount:
+                                state.reportModel[0].studentsList?.length,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            await createExcelFile(
+                                state.reportModel[0].studentsList!);
+                            if (context.mounted) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                GlobalSnackBar.show(context,
+                                    'Excel file created successfully! , saved to downloads'),
+                              );
+                            }
+                          },
+                          style: ButtonStyle(
+                            fixedSize: WidgetStateProperty.all(
+                              Size(246.w, 53.h),
+                            ),
+                            shape: WidgetStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                  16.w,
+                                ),
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            getAppLocalizations(context)!.downloadFullList,
+                            style: GoogleFonts.roboto(
+                              textStyle: TextStyle(
+                                fontSize: 20.sp,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
                 } else if (state is GetLecturesReportsFailure) {
                   return Center(child: Text(state.errMessage));
                 } else {
